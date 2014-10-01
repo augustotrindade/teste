04183e3f83b4ae6a707e146cd40c2b78e09f0008
Augusto Trindade <augustotrindade@gmail.com>
2014-10-01 16:48:05 -0300

    remove linha

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 include 'cabecalho.php';
 echo "olá github, e mais alguma coisa, ok ok sdikjfbsiduf";
-echo "<br/>";
-echo "<hr/>";
-echo "<br/>";
 echo "mais um teste";
 include 'rodape.php';
 ?>
