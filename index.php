c947f429b7b889466b9a40948936374235edde2e
Augusto Trindade <augustotrindade@gmail.com>
2015-01-08 21:10:25 -0200

    teste teste

--- index.php
+++ index.php
@@ -7,5 +7,5 @@ $tbs->LoadTemplate($doc_root.'/templates/index.html');
 $tbs->MergeBlock('carros', $m->adb->carro->find(array('_id'=>array('$in'=>(isset($_SESSION['usuario']['carros']) ? $_SESSION['usuario']['carros'] : array()))))) ;
 $tbs->Show();
 
-//teste de git ZEND
+//teste de git ZEND.
 ?>
