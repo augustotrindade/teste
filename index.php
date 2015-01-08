17c5a610828f56e2d11da2c38996f7ecac84c49b
Augusto Trindade <augustotrindade@gmail.com>
2015-01-08 10:38:06 -0200

    Update index.php
    alterando a mesma linha

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include './scripts/config.php';
 include './scripts/restrito.php';
-//so um teste
+//meu teste
 $tbs->LoadTemplate($doc_root.'/templates/index.html');
 
 $tbs->MergeBlock('carros', $m->adb->carro->find(array('_id'=>array('$in'=>(isset($_SESSION['usuario']['carros']) ? $_SESSION['usuario']['carros'] : array()))))) ;
